6469707112bd7aa789b0a43e988d0ee065be02c4
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-06 14:01:16 +0600

    add

---
+++ array-loop.js
@@ -0,0 +1,29 @@
+const fruits = ["apple", "banana", "kiwi", "lemon"];
+// console.log(fruits[0]);
+for ( let i = 0; i < fruits.length; i++){
+    // console.log(fruits[i]);
+    // console.log(i);
+}
+
+
+const numbers = [12, 23 , 34, 45, 67, 87];
+for (let j = 0; j < numbers.length; j++){
+    // console.log(numbers[j]);
+}
+
+
+
+let k = 0;
+while(k < fruits.length){
+    // console.log(fruits[k]);
+    k++;
+}
+
+
+
+const prices = [1221, 3435, 4567, 434, 564, 546];
+let n = 0;
+while(n < prices.length){
+    console.log(prices[n]);
+    n++;
+}
