d79a56830eae6ccc069f25684286927d16b535b5
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-06 15:26:55 +0600

    add

---
+++ object-array.js
@@ -0,0 +1,20 @@
+const numbers = [1, 2, 3, 4, 5];
+const players = [{}, {}, {}];
+const employees = [
+    {name: "Shafin", designation: "Research Assistant", salary: 30000 },
+    {name: "Nafin", designation: "Teaching Assistant", salary: 24000 },
+    {name: "Anik", designation: "Marketing Manager", salary: 20000 },
+    {name: "Archo", designation: " Web Developer", salary: 40000 }
+];
+console.log(employees[3].salary);
+
+// for of
+for(const employee of employees){
+    console.log(employee.salary, employee.designation);
+}
+
+// variable
+for(const employee of employees){
+    const emp_salary = employee.salary;
+    console.log(emp_salary);
+}
